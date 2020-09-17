b80bcfbf70330fa8db697373be138fd89c758756
Albin Eriksson <albineriksson100@gmail.com>
2020-09-17 10:53:08 +0200

    Update SLogin.php

--- SLogin.php
+++ SLogin.php
@@ -1,7 +1,8 @@
 <?php
-// @author Albin Eriksson, https://github.com/kezoponk
-// @license MIT, https://opensource.org/licenses/MIT
-
+/**
+ * @author Albin Eriksson, https://github.com/kezoponk
+ * @license MIT, https://opensource.org/licenses/MIT
+ */
 class Credentials {
   function __construct() {
     // Database credentials
